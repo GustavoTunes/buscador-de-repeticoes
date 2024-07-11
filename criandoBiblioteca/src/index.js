dd6784fcbee1d057c09276a16824cc26e47d0935
GustavoTunes <gustavosouzatunes@gmail.com>
2024-07-11 09:23:53 -0300

    Introdução ao node.js

---
+++ criandoBiblioteca/src/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const caminhoArquivo = process.argv;
+const link = caminhoArquivo[2];
+
+fs.readFile(link, 'utf-8', (erro, texto) => {
+    verificaDuplicadas(texto);
+});
+
+function verificaDuplicadas(texto) {
+    const listaPalavras = texto.split(' ');
+    const resultado = {};
+    listaPalavras.forEach(palavra => {
+        resultado[palavra] = (resultado[palavra] || 0) + 1;
+    });
+    console.log(resultado);
+}
