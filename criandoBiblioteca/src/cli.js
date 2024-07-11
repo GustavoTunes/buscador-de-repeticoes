37901f0bfded5f597691d84667cdd21f7a72e9c7
GustavoTunes <gustavosouzatunes@gmail.com>
2024-07-11 16:59:41 -0300

    Implementação de bibliotecas externas

--- criandoBiblioteca/src/cli.js
+++ criandoBiblioteca/src/cli.js
@@ -1,28 +1,59 @@
 import fs from 'fs';
+import path from 'path';
 import trataErros from './erros/funcoesErro.js';
 import { contaPalavras } from './index.js';
 import { saidaArquivo } from './helpers.js';
+import { Command } from 'commander';
+import chalk from 'chalk';
 
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2];
-const endereco = caminhoArquivo[3];
+const program = new Command();
 
-fs.readFile(link, 'utf-8', (erro, texto) => {
-    try {
-        if (erro) throw erro;
-        const resultado = contaPalavras(texto);
-        criaSalvaArquivo(resultado, endereco);
-    } catch (erro) {
-        trataErros(erro);
-    }
-});
+program
+    .version('0.0.1')
+    .option('-t, --texto <string>', 'caminho do texto a ser processado')
+    .option('-d, --destino <string>', 'caminho da pasta onde será salvo o arquivo resultado')
+    .action((options) => {
+        const { texto, destino } = options;
+
+        if (!texto || !destino) {
+            console.error(chalk.red('Erro: Insira o caminho de origem e destino!'));
+            program.help();
+            return;
+        }
+
+        const caminhoTexto = path.resolve(texto);
+        const caminhoDestino = path.resolve(destino);
+
+        try {
+            processaArquivo(caminhoTexto, caminhoDestino);
+            console.log(chalk.yellow('Texto processado'));
+        } catch (erro) {
+            console.log(chalk.red('Erro no processamento:'));
+        }
+    });
+
+program.parse();
+
+function processaArquivo(texto, destino) {
+    fs.readFile(texto, 'utf-8', (erro, texto) => {
+        try {
+            if (erro) throw erro;
+            const resultado = contaPalavras(texto);
+            criaSalvaArquivo(resultado, destino);
+        } catch (erro) {
+            trataErros(erro);
+        }
+    });
+}
+
+
 
 async function criaSalvaArquivo(listaPalavras, endereco) {
     const arquivoNovo = `${endereco}/resultado.txt`;
     const textoPalavras = saidaArquivo(listaPalavras);
     try {
         await fs.promises.writeFile(arquivoNovo, textoPalavras);
-        console.log('arquivo criado');
+        console.log(chalk.green('arquivo criado'));
     } catch (erro) {
         throw erro;
     }
